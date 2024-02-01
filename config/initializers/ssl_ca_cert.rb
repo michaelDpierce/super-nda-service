c32a954a308c1aa854d3b4d0422f549552da1c38
michaelDpierce <matchmike1313@gmail.com>
2024-02-01 14:22:50 -0700

    Prod cert

--- config/initializers/ssl_ca_cert.rb
+++
@@ -1,9 +0,0 @@
-# ==============================================================================
-# Copyright 2024, MinuteBook. All rights reserved.
-# ==============================================================================
-
-if ENV['SSL_CA_CERT_BASE64']
-  decoded_cert = Base64.decode64(ENV['SSL_CA_CERT_BASE64'])
-  File.write('tmp/ca_cert.pem', decoded_cert)
-  ENV['SSL_CA_CERT_PATH'] = Rails.root.join('tmp', 'ca_cert.pem').to_s
-end
