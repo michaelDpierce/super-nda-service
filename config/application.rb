beb7d51c6f3338d14ecb961b333f000a6edaf1c9
michaelDpierce <matchmike1313@gmail.com>
2024-02-02 11:55:48 -0700

    Action cable issue

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'action_mailer/railtie'
 # require 'action_mailbox/engine'
 # require 'action_text/engine'
 require 'action_view/railtie'
-# require 'action_cable/engine'
+require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
